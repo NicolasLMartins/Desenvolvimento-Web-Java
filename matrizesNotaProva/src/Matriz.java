182088736e355a6e1a0bde6effb0924de6971e2e
NicolasLMartins <116754178+NicolasLMartins@users.noreply.github.com>
2023-05-12 16:25:51 +0000

    update matrizNotaProva (completo)

--- matrizesNotaProva/src/Matriz.java
+++ matrizesNotaProva/src/Matriz.java
@@ -1,3 +1,14 @@
 public class Matriz {
-    
+    static void mostrarMatriz(Float[][] matriz){
+        for (int i = 0; i < matriz.length; i++) {
+            
+            System.out.print(String.format("Notas do aluno #%d: ", i + 1));
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(String.format("Nota %d/4: |%-5.2f| ", j + 1, matriz[i][j]));
+                // System.out.print(matriz[i][j]);
+            }
+
+            System.out.println();
+        }
+    }
 }
--- matrizesNotaProva/src/Principal.java
+++ matrizesNotaProva/src/Principal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner ler = new Scanner(System.in);
+        Float[][] matriz = new Float[5][4];
+
+        System.out.println("Digite as notas dos alunos: ");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.println(String.format("Aluno %d: ", i + 1));
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(String.format("Nota %d: ", j + 1));
+                matriz[i][j] = ler.nextFloat();
+                // matriz[i][j] = "|t|";
+            }
+        }
+        ler.close();
+
+        Matriz.mostrarMatriz(matriz);
     }
 }
